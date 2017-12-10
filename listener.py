0e604c9be0e49506d790feca333ca31e067a579c
dexter aparicio <truthadjustr@gmail.com>
2017-12-10 17:46:04 +0800

    init

---
+++ listener.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+
+Usage::
+    ./dummy-web-server.py [<port>]
+
+Send a GET request::
+    curl http://localhost
+
+Send a HEAD request::
+    curl -I http://localhost
+
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        #f = open("/dev/stdout","w")
+        # Doesn't do anything with posted data
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        #print(post_body)
+
+        python_obj = json.loads(post_body)
+        try:
+            for msg in python_obj["check_result"]["matching_messages"]:
+                #f.write(msg["message"] + "\n")
+                print(msg["message"] + "\n")
+        except:
+            #f.write("missing field\n")
+            print("missing field")
+        #f.write(post_body)
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+        #f.write("\n--------\n")
+        print("\n--------\n")
+        #f.close()
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
